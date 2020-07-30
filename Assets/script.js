9f3e1047e94e3b79c901a8c6d0a7c81132376764
Nick Guimarais <Nick.guimarais@gmail.com>
2020-07-29 20:41:08 -0700

    created psuedocode for homework assignment, added index.html, style.css, and script.js file

---
+++ Assets/script.js
@@ -0,0 +1,24 @@
+/* This is my psuedo code
+HTML
+1. develop HTML Structure of Code Quiz
+
+CSS
+2. develop styling for Code Quiz, get creative
+
+Questions
+-develop questions for the code quiz
+
+JavaScript
+3.  declare countdown variables
+4.  addEventListerner ("click") to start button to begin function.
+4a. I will need a variable set to the value of the function called set Interval, see ex 8.
+4b. in the function we will decrement the countdown variable and present a question (i.e. append element <p> containing question in the function);
+4c. I will also need to append an element that has a list of answers that are clickable <ul> of buttons </ul>;
+4d. I will need to incorporate an add an event lister to the question and when clicked, the If statement in the function to determine if the answer is correct, increment score and display correct, otherwise subtract time (decrement) and display wrong;
+4e. I will need to have the element (question) removed each time the user clicks a button and appends the new question.
+5.  when all questions are answered or || countdown = 0, exit out of the function and call a new function called Score.
+6.  Score function will append an input field for their initials where they can save their score.
+6a. when the save button is clicked for user's score, the element will be removed
+7.  I will have to create and append another input field for highscore, use set item and get item from local storage so the highscore can be saved. remember JSON Stringify and parse.
+
+*/
